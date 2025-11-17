1be0433be40d2d3b36bb03554f018c824a298a04
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2025-11-17 18:05:08 -0500

    created task presenter

--- src/main/java/interface_adapter/modify_task/ModifyTaskPresenter.java
+++ src/main/java/interface_adapter/modify_task/ModifyTaskPresenter.java
@@ -1,9 +1,14 @@
 package interface_adapter.modify_task;
 
+import interface_adapter.ViewManagerModel;
 import use_case.modify_task.ModifyTaskOutputBoundary;
 import use_case.modify_task.ModifyTaskOutputData;
 
 public class ModifyTaskPresenter implements ModifyTaskOutputBoundary {
+
+
+    public ModifyTaskPresenter(ViewManagerModel viewManagerModel, ViewManaviewTaskModel) {
+    }
 
     @Override
     public void prepareSuccessView(ModifyTaskOutputData outputData) {
