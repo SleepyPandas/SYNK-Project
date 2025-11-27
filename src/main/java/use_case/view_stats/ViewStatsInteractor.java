dd16383d4da6e88c50188072df9d7bbf1394cd87
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2025-11-27 09:40:45 -0500

    fix typo and updated empty max habit streak

--- src/main/java/use_case/view_stats/ViewStatsInteractor.java
+++ src/main/java/use_case/view_stats/ViewStatsInteractor.java
@@ -25,7 +25,7 @@ public class ViewStatsInteractor implements ViewStatsInputBoundary {
         final ArrayList<Task> taskList = taskGateway.fetchTasks(inputData.getUsername());
         final ArrayList<Habit> habitList = habitGateway.fetchHabits(inputData.getUsername());
 
-        int longestHabitStreak = -1;
+        int longestHabitStreak = 0;
         int numHabitsCompleted = 0;
         for (Habit habit : habitList) {
             if (habit.getStreakCount() > longestHabitStreak) {
@@ -43,7 +43,7 @@ public class ViewStatsInteractor implements ViewStatsInputBoundary {
         }
 
         final int totalTasks = taskList.size();
-        final int totalHabits = taskList.size();
+        final int totalHabits = habitList.size();
 
         final ViewStatsOutputData viewStatsOutputData = new ViewStatsOutputData(longestHabitStreak, numTasksCompleted,
                 numHabitsCompleted, totalTasks, totalHabits);
